ec80b2ea729e5898c7b6f5a34ba3a004403a366a
John Tyndall <john@tyndall.io>
2021-11-04 17:05:33 -0400

    Add Player class.

---
+++ Engine/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace Engine
+{
+    public class Player
+    {
+        public int CurrentHitPoints { get; set; }
+
+        public int MaximumHitPoints { get; set; }
+
+        public int Gold { get; set; }
+
+        public int ExperiencePoints { get; set; }
+
+        public int Level { get; set; }
+    }
+}
